a1af46b5111e0ae650f90d4be4a518b096e4a4dd
girudik <102294076+girudik@users.noreply.github.com>
2024-11-11 23:06:14 +0300

    о

---
+++ db/dbmain.php
@@ -0,0 +1,30 @@
+<?php
+
+Class dbRead{
+    #posts
+    function dbGetPost
+    function dbGetThread
+    function dbGetBoard
+    function dbGet
+}
+Class dbWrite{
+    #posts
+    function dbCreatePost
+        #global
+    function dbMarkAsDeletedPost
+        #global
+    function dbDeletePost
+    
+    function dbCreateThread
+
+    function dbCreateBoard
+    
+    function dbDeletePost
+        #janie
+
+    function dbEditPost
+        #janie
+
+}
+
+?>
